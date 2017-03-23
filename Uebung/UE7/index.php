c6e68f3bdcb167b7b387d33e66e996c254895ea0
Paul Camerloher <paul.camerloher@gmail.com>
2017-03-23 14:53:00 +0100

    added add button

--- Uebung/UE7/index.php
+++ Uebung/UE7/index.php
@@ -19,7 +19,15 @@ $db = new PDO("mysql:host={$host};dbname={$dbname}", $user, $pass);
 
 if(isset($_POST["submit"]))
 {
-    $db->query("UPDATE project SET title ='".$_POST["title"]."', description ='".$_POST["desc"]."', creationDate ='".$_POST["date"]."' WHERE id =".$_POST["id"]);
+    if(!$_POST["id"]=="") {
+        $db->query("UPDATE project SET title ='" . $_POST["title"] . "', description ='" . $_POST["desc"] . "', creationDate ='" . $_POST["date"] . "' WHERE id =" . $_POST["id"]);
+    }
+    else
+    {
+        $var = "INSERT INTO project (title, description, creationDate, user_id) VALUES ('".$_POST["title"]."', '".$_POST["desc"]."', '".$_POST["date"]."', 1)";
+        ///echo $var;
+        var_dump($db->query($var));
+    }
 }
 
 if(isset($_GET["deleteID"]))
@@ -35,7 +43,6 @@ $statement = "SELECT count(*) FROM project";
 $res = $db->query($statement);
 $tmp = $res->fetchAll(PDO::FETCH_NUM);
 $count = $tmp[0][0];
-echo $count;
 $statement = "SELECT p.id,p.title,u.username,p.description,p.creationDate, p.state FROM project p JOIN user u ON u.id = p.user_id LIMIT ".(($_GET['side']-1) * 20).", 20;";
 $res = $db->query($statement);
 $tmp = $res->fetchAll(PDO::FETCH_ASSOC);
@@ -76,72 +83,96 @@ $tmp = $res->fetchAll(PDO::FETCH_ASSOC);
 
         $num1 = 1;
         $num2 = 1;
-
-        if($_GET['side'] <= 2)
-        {
-            $num1 = 1;
-        }
-        else
-        {
-            $num1 = $_GET['side']-2;
-        }
-
-        if($_GET['side'] <= 1)
-        {
-            $num2 = 1;
-        }
-        else
-        {
-            $num2 = $_GET['side']-1;
-        }
-    
-        echo "<a href = 'index.php?side=" . ($num1) . "&amount=20'><<</a>";
-        echo "  ";
-        echo "<a href = 'index.php?side=" . ($num2) . "&amount=20'><</a>";
-        echo "  ";
         $offset = 0;
         if($count % 20 != 0)
         {
             $offset = 1;
         }
-        for($i = 1; $i < $count / 20 + $offset; $i++)
+
+
+        if($_GET['side'] <= 1)
         {
-            echo "<a href = 'index.php?side=".$i."&amount=20'>".$i." </a>";
+            $num1 = 1;
         }
-        echo "  ";
-        echo "<a href = 'index.php?side=".($_GET['side']+1)."&amount=20'>></a>";
-        echo "  ";
-        echo "<a href = 'index.php?side=".($_GET['side']+2)."&amount=20'>>></a>";
+        else{
+        $num1 = $_GET['side']-1;}
+
+
+        if($_GET['side'] >= $count / 20 + $offset)
+        {
+            $num2 = $count / 20 + $offset;
+        }
+        else
+        {
+            $num2 = $_GET['side']+1;
+        }
+
+        echo "<ul class = 'pagination'>";
+        echo "<li class='Previous'><a href = 'index.php?side=" . ($num1) . "&amount=20' aria-label=\"Previous\">
+        <span aria-hidden=\"true\">&laquo;</span>
+      </a></li>";
+
+        for($i = 1; $i < $count / 20 + $offset; $i++) {
+            echo "<li><a href = 'index.php?side=" . $i . "&amount=20'>" . $i . " </a></li>";
+        }
+        echo "<li class='Next'><a href = 'index.php?side=" . ($num2) . "&amount=20' aria-label=\"Next\">
+        <span aria-hidden=\"true\">&raquo;</span>
+      </a></li>";
+        echo "</ul>";
 
         ?>
     </div>
 
     <?php
-        if(isset($_GET["updateID"]))
-        {
-            $res = $db->query("SELECT p.id,p.title,u.username,p.description,DATE_FORMAT(creationDate, '%Y-%m-%dT%H:%i'), p.state FROM project p JOIN user u ON u.id = p.user_id WHERE p.id =".$_GET["updateID"].";");
+    echo "<a href = 'index.php'><button>Add</button></a>";
+/*$title ="";
+        $description = "";
+        $creationDate = "";
+        $id = "";
+        $button = "Add";*/
+        if(isset($_GET["updateID"])) {
+            $res = $db->query("SELECT p.id,p.title,u.username,p.description,creationDate, p.state FROM project p JOIN user u ON u.id = p.user_id WHERE p.id =" . $_GET["updateID"] . ";");
             $tmp = $res->fetchAll(PDO::FETCH_ASSOC);
-
+            //var_dump($tmp);
+         /*   $title = $tmp[0]['title'];
+            $description = $tmp[0]['description'];
+            $creationDate = $tmp[0]['creationDate'];
+            $id = $tmp[0]['id'];
+            $button = "Update";*/
+            PrintFormFull($tmp[0]['title'],$tmp[0]['description'],$tmp[0]['creationDate'],$tmp[0]['id'],"Update");
+        }
+        else
+        {PrintForm();}
+        function PrintForm()
+        {
+            PrintFormFull("","","","","");
+        }
+function PrintFormFull($title,$description,$creationDate,$id,$button)
+{ 
             echo "<form action='index.php' method='post'>
         <div class=\"form-group\">
             <label for=\"n\">Name</label>
-            <input type=\"text\" class=\"form-control\" name='title' value=\"".$tmp[0]["title"]."\">
+            <input type=\"text\" class=\"form-control\" name='title' value=\"".$title."\">
         </div>
         <div class=\"form-group\">
             <label for=\"d\">Description</label>
-            <input type=\"text\" class=\"form-control\" name='desc' value=\"".$tmp[0]["description"]."\">
-        </div>
+            <input type=\"text\" class=\"form-control\" name='desc' value=\"".$description."\">
+        </div>";
+    if($creationDate == "")
+    {
+    echo "
         <div class=\"form-group\">
             <label for=\"da\">Date</label>
-            <input type=\"date\" class=\"form-control\" name='date' value=\"".$tmp[0]["creationDate"]."\">
-        </div>
+            <input type=\"date\" class=\"form-control\" name='date' value=\"".$creationDate."\">
+        </div>";}
+    echo "
         <div class=\"form-group\">
             <label for=\"u\">Update</label>
-            <input type=\"submit\" class=\"form-control\" name='submit' value=\"Update\">
-            <input type=\"hidden\" name='id' value=\"".$tmp[0]["id"]."\">
+            <input type=\"submit\" class=\"form-control\" name='submit' value=\"".$button."\">
+            <input type=\"hidden\" name='id' value=\"".$id."\">
         </div>
     </form>";
-        }
+}
     ?>
 
 
